327084c82582ce2e54e456b2fa7e5693c1fce726
Jamyth Ariza <jamythariza@gmail.com>
2020-08-27 10:52:37 -0500

    Ajustes de css, jquery para mostrar el menu al cargar.

--- js/main.js
+++ js/main.js
@@ -18,70 +18,8 @@
 		
 		this.init();	
 	};
-	
-	Progress.prototype = {
-		init: function() {
-			var self = this;
-			self.timer = setInterval(function() {
-				self.run();	
-			}, 25);
-		},
-		run: function() {
-			var self = this;
-			
-			self.diff = ( ( self.loaded / 100 ) * Math.PI * 2 * 10 ).toFixed( 2 );	
-			self.context.clearRect( 0, 0, self.width, self.height );
-			self.context.lineWidth = 4;
-			self.context.fillStyle = "#000";
-			self.context.strokeStyle = "#D6AC4B";
-			self.context.textAlign = "center";
-			
-			self.context.fillText( self.loaded + "%", self.width * .5, self.height * .5 + 2, self.width );
-			self.context.beginPath();
-			self.context.arc( 35, 35, 30, self.start, self.diff / 10 + self.start, false );
-			self.context.stroke();
-			
-			if( self.loaded >= self.total ) {
-				clearInterval( self.timer );
-			}
-			
-			self.loaded++;
-		}
-	};
-	
-	var CircularSkillBar = function( elements ) {
-		this.bars = document.querySelectorAll( elements );
-		if( this.bars.length > 0 ) {
-			this.init();
-		}	
-	};
-	
-	CircularSkillBar.prototype = {
-		init: function() {
-			this.tick = 25;
-			this.progress();
-			
-		},
-		progress: function() {
-			var self = this;
-			var index = 0;
-			var firstCanvas = self.bars[0].querySelector( "canvas" );
-			var firstProg = new Progress( firstCanvas );
-			
-			var timer = setInterval(function() {
-				index++;
-					
-				var canvas = self.bars[index].querySelector( "canvas" );
-				var prog = new Progress( canvas );
-				
-				if( index == self.bars.length ) {
-						clearInterval( timer );
-				} 
-				
-			}, self.tick * 100);
-				
-		}
-	};
+		
+
 var button = document.getElementById("softs");
   
 	button.addEventListener( "click", function() {
@@ -91,28 +29,6 @@ var button = document.getElementById("softs");
 		var circularBars = new CircularSkillBar( "#hbars .hbar" );
 	});
 })();
-
-
-$(document).ready(function () { 
-	$("#hards").click(function(){
-		$(".sbar")
-            .filter(function(){ return ! $(this).is(":hidden"); })
-            .fadeOut(200, function(){
-				$('.hbar,.hbars').fadeIn(300);
-			});
-			$('#softs').removeClass("bactive");
-			$('#hards').addClass("bactive");		
-	});
-	$("#softs").click(function(){
-		$(".hbar")
-            .filter(function(){ return ! $(this).is(":hidden"); })
-            .fadeOut(200, function(){
-				$('.sbar,.sbars').fadeIn(300);
-			});
-			$('#hards').removeClass("bactive");
-			$('#softs').addClass("bactive");
-	});
-});
 
 window.sr = ScrollReveal();
 	sr.reveal('.logo', {
@@ -124,10 +40,6 @@ window.sr = ScrollReveal();
 		duration: 2000,
 		origin: 'bottom'
 	});
-	sr.reveal('.navbar', {
-		duration: 2000,
-		origin: 'bottom'
-	});
 	sr.reveal('.header-content-left', {
 		duration: 2000,
 		origin: 'top',
@@ -135,7 +47,7 @@ window.sr = ScrollReveal();
 	});
 	sr.reveal('.header-content-right', {
 		duration: 2000,
-		origin: 'right',
+		origin: 'bottom',
 		distance: '300px'
 	});
 	sr.reveal('.header-btn', {
@@ -145,8 +57,9 @@ window.sr = ScrollReveal();
 	});
 	sr.reveal('#testimonial', {
 		duration: 2000,
-		origin: 'left',
-		viewFactor: 0.2
+		origin: 'bottom',
+		viewFactor: 0.2,
+		distance: '300px'
 	});
 	sr.reveal('.info-left', {
 		duration: 2000,
@@ -155,7 +68,7 @@ window.sr = ScrollReveal();
 	});
 	sr.reveal('.info-right', {
 		duration: 2000,
-		origin: 'right',
+		origin: 'top',
 		distance: '300px'
 	});
 	sr.reveal('.info-btn', {
@@ -165,16 +78,15 @@ window.sr = ScrollReveal();
 	});
 	sr.reveal('#info-two', {
 		duration: 2000,
-		origin: 'left',
-		distance: '300px',
-		viewFactor: 0.2
+		origin: 'bottom',
+		distance: '300px'
 	});
 	sr.reveal('#contact', {
 		duration: 2000,
 		origin: 'bottom',
-		distance: '300px',
-		viewFactor: 0.2
+		distance: '100px'
 	});
+	
 
 	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
